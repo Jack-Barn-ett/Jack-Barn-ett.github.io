1c56bf13d2dd31c50a370ce0c19c566030874114
JBarnett100 <JBarnett10000@gmail.com>
2023-06-12 21:41:33 -0500

    Reloading old changes

---
+++ svelte-app/src/components/Header.svelte
@@ -0,0 +1,21 @@
+<script>
+  let title = "Splash Image";
+</script>
+
+<div style="background-image: url('/images/header_splash_image.png'); background-size: cover;">
+  <h1 class="header-title">{title}</h1>
+</div>
+
+<style>
+  .header {
+    background-image: url('/images/header_splash_image.png');
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    height: 200px;
+  }
+</style>
+
+<div class="header">
+  <!-- header content -->
+</div>
---
+++ svelte-app/src/components/SideMenu.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { slide } from 'svelte/transition';
+  let open = false;
+  let projects = ['Project 1', 'Project 2', 'Project 3'];
+</script>
+
+<button on:click={() => open = !open}>
+  {open ? 'Hide' : 'Show'} Projects
+</button>
+
+{#if open}
+  <ul transition:slide>
+    {#each projects as project (project)}
+      <li>{project}</li>
+    {/each}
+  </ul>
+{/if}
+
+<style>
+  ul {
+    list-style-type: none;
+    padding-left: 0;
+    margin-top: 15px;
+  }
+  button {
+    margin-top: 15px;
+  }
+</style>
